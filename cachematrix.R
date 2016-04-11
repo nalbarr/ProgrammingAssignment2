41a29cb2a90c4c951678c35475d5fb50daa9c061
nalbarracin <nalbarracin@ontoadaptive.com>
2016-04-10 22:21:25 -0500

    Commit Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Caching the Inverse of a Matrix
+## 
 
-## Write a short comment describing this function
+## Function:  makeCacheMatrix
+## Creates a specialized extended matrix data type with additional functions to support accessor functions
+## to support caching.
+## Args:
+## - matrix
+## Returns: 
+## - list of accessor functions as set, get, setmatrix, getmatrix
+## Error handling
+## - None.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmatrix <- function(matrix) m <<- matrix
+  getmatrix <- function() m
+  list(set = set, get = get,
+       setmatrix = setmatrix,
+       getmatrix = getmatrix)
 }
 
-
-## Write a short comment describing this function
+## Function:  cacheSolve
+## Creates a specialized extended matrix data type with additional functions to support accessor functions
+## to support caching.
+## Args:
+## - cahematrix as a list of functions
+## Returns: 
+## - matrix
+## Error handling
+## - Input matrix results in undeteriminant inverse function.  Guard against divide by zero.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$getmatrix()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  if (det(data) != 0) {
+    m <- solve(data, ...)
+    x$setmatrix(m)
+  } else {
+    message("Can not solve determinant of input matrix returns zero.")
+  }
+  m  
 }
